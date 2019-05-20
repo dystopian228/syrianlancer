2159a9c2c4e60fd8d1a6c1725dc38b7b98f2e3f7
Ghostazarashi <SpartanGhost228R0@gmail.com>
2019-05-20 23:05:17 +0300

    add: main, header and footer pages

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./stylesheets/bootstrap.min.css" crossorigin="anonymous">
+    <script src="./js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./stylesheets/main.css">
+    <title>SyrianLancer</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#!">SyrianLancer</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#nav-items" aria-controls="nav-items" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-items">
+            <ul class="nav navbar-right ml-auto mt-4 mb-4 mr-4">
+                <li class="nav-item"><button type="button" class="btn btn-outline-primary">تسجيل الدخول</button></li>
+                <li class="nav-item"><button type="button" class="btn btn-outline-primary">تسجيل حساب</button></li>
+                <li class="nav-item"><a class="nav-link" href="#">أضف عمل</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">تصفح الأعمال</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">تصفح المستقلين</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
