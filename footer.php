d97345c7dd635960290ee801e12f5059feeb681c
Muraf Shaker <SpartanGhost228R0@gmail.com>
2019-05-21 23:15:12 +0300

    add: footer
    fix: nonfunctional collapse dropdown
    add: boilerplate lorem ipsum to index

--- footer.php
+++ footer.php
@@ -1,2 +1,39 @@
+<footer>
+<div class="container">
+    <div class="row footer">
+        <div class="col-lg-4">
+            <h5>SyrianLancer</h5>
+            <ul class="footer-items">
+                <li><a href="#">عن Syrianlancer</a></li>
+                <li><a href="#">شروط الاستخدام</a></li>
+                <li><a href="#">سياسة الخصوصية</a></li>
+                <li><a href="#">تواصل معنا</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+        <h5>الأعمال</h5>
+            <ul class="footer-items">
+            <li><a href="#">الأعمال البرمجية</a></li>
+            <li><a href="#">الأعمال القانونية</a></li>
+            <li><a href="#">الأعمال التصميمية</a></li>
+            <li><a href="#">الأعمال التسويقية</a></li>
+            <li><a href="#">الأعمال الأدبية</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+        <h5>روابط مهمة</h5>
+            <ul class="footer-items">
+            <li><a href="#">معرض الأعمال</a></li>
+            <li><a href="#">حسابي</a></li>
+            <li><a href="#">مركز مساعدة المشترك</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="row copyright p-4">
+    <div class="col-sm-6 copyright-en">SyrianLancer © 2019</div>
+    <div class="col-sm-6 copyright-ar">سيريان لانسر © 2019</div>
+</div>
+</footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,20 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./stylesheets/bootstrap.min.css" crossorigin="anonymous">
+    <script src="./js/jquery3_4.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="./stylesheets/main.css">
     <title>SyrianLancer</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#!">SyrianLancer</a>
+    <a class="navbar-brand" href="index.php">SyrianLancer</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
             data-target="#nav-items" aria-controls="nav-items" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="nav-items">
             <ul class="nav navbar-right ml-auto mt-4 mb-4 mr-4">
-                <li class="nav-item"><button type="button" class="btn btn-outline-primary">تسجيل الدخول</button></li>
+                <li class="nav-item"><button type="button" onClick="document.location.href='login.php'" class="btn btn-outline-primary">تسجيل الدخول</button></li>
                 <li class="nav-item"><button type="button" class="btn btn-outline-primary">تسجيل حساب</button></li>
                 <li class="nav-item"><a class="nav-link" href="#">أضف عمل</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">تصفح الأعمال</a></li>
@@ -26,3 +27,4 @@
             </ul>
         </div>
     </nav>
+
