6a92d6238cc23d49045d53b61406839054bb6707
Ali Alaa Eddin <AliAlaaEddin1@gmail.com>
2019-05-22 23:44:35 +0300

    edit login page style

--- login.php
+++ login.php
@@ -2,14 +2,17 @@
     require 'header.php';
 ?>
 
-    <div class="login-div">
-        اسم المستخدم: 
-        <input type="text" name="username" id="username">
-        كلمة السر:
-        <input type="password" name="password"" id="pwd">
-        <button id="login-button">تسجيل الدخول</button>
+<main class="login-main">
+    <div class="login-div col-md-4 col-centered">
+        <h1 >تسجيل الدخول</h1>
+        <p>البريد الإلكتروني:</p> 
+        <input type="text" name="username" id="username" class="form-control text-box-login" placeholder="أدخل البريد الإلكتروني الخاص بك">
+        <p>كلمة المرور:</p> 
+        <input type="password" name="password" id="pwd" class="form-control text-box-login" placeholder="أدخل كلمة المرور الخاصة بك"><br>
+        <button id="login-button" class="btn btn-primary login-btn">دخول</button>
     </div>
-
+</main>
+    
 <?php 
     require 'footer.php';
 ?>
