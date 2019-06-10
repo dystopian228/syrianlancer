a6ecd4f8728d29885d60cbdaeec718cd87aea1ef
fatima <fatemaaldabaa@gmail.com>
2019-06-10 07:49:51 +0300

    add project

--- addProject.php
+++ addProject.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['userID'])){
         <div class="row mb-2">
             <div class="col-md-6 mb-4">
                 <p>المهارات المطلوبة</p>
-                <select  id="skills" class=" border border-warning text-box-addProject skills-select">
+                <select  id="category" name="category" class=" border border-warning text-box-addProject skills-select">
                     <option value="">خدمات استشارية و إدارية</option>
                     <option value=""> برمجيات, تصميم مواقع وتطبيقات</option>
                     <option value="">تسويق ومبيعات</option>
@@ -33,20 +33,28 @@ if(isset($_SESSION['userID'])){
         <div class="row">
             <div class="col-md-6 md-form">
                 <p>وصف المشروع</p>
-                <textarea id="textarea-char-counter" class="border border-warning  md-textarea text-box-addProject " length="200" rows="8"></textarea>
+                <textarea id="textarea-char-counter" name ="description" class="border border-warning  md-textarea text-box-addProject " length="200" rows="8"></textarea>
                 <label for="inputEmail4MD" class="margin-bottom-addProject">أكتب توصيفا لمشروعك</label>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6">
                 <p>الميزانية</p>
-                <input type="text"  id="pName" class="border border-warning form-control  " placeholder="">
-                <label for="inputEmail4MD" class="margin-bottom-addProject">أدخل ميزانية تتناسب مع مشروعك</label>
+                <select  id="palance" name="palance" class=" border border-warning text-box-addProject form-control skills-select">
+                    <option value="">15-25 ألف</option>
+                    <option value=""> 25-50 ألف</option>
+                    <option value="">50-125 ألف</option>
+                    <option value=""> 125-250 ألف</option>
+                    <option value=""> 250-400 ألف</option>
+                    <option value=""> 400-500 ألف</option>
+                </select>
+               
+                <label for="inputEmail4MD" class="margin-bottom-addProject">اختر ميزانية تتناسب مع مشروعك</label>
             </div>
     
             <div class="col-md-6">
                 <p>مدة التسليم</p>
-                <input type="text"  id="pName" class="border border-warning form-control " placeholder="">
+                <input type="text" name="duration" class="border border-warning form-control " placeholder="">
                 <label for="inputEmail4MD" class="margin-bottom-addProject">أدخل المدة الزمنية الكافية لعمل المشروع</label>
             </div>
         </div>
